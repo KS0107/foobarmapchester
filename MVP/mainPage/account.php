e70efb6af442c288404d09f753f96a2a76000685
George Goodey <george.goodey@student.manchester.ac.uk>
2022-02-24 11:23:26 +0000

    Rearranged Files

---
+++ MVP/mainPage/account.php
@@ -0,0 +1,59 @@
+<?php
+function addUser($username, $password)
+{	// insert username and password into users table  
+	$sql = "INSERT INTO user (username, password) 
+			VALUES (:username, :password)";
+	// connect to the database
+	$pdo = new pdo('mysql:host=dbhost.cs.man.ac.uk; dbname=2021_comp10120_z19', 'y02478jh', 'Hjy20011222.');
+	// Set  attributes
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); 
+	// Hash password
+	$password = password_hash($password, PASSWORD_DEFAULT);
+	// Execution Part. This prevents SQL injection; 
+	//提供给预处理语句的参数不需要用引号括起来, ，驱动程序会自动处理。
+	$stmt = $pdo->prepare($sql);
+	// bind parameters with variables
+	$stmt->execute([
+						'username' => $username,
+						'password' => $password
+						]);
+}
+
+function showDatabases()
+{
+   $sql = "SHOW DATABASES";
+   $pdo = new pdo('mysql:host=dbhost.cs.man.ac.uk;',
+                 'y02478jh', 'Hjy20011222.');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+   $stmt = $pdo->prepare($sql);
+   $stmt->execute();
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+   while ($row = $stmt->fetch())
+   {
+        print("<h3>" . $row['Database'] . "</h3>");
+    }
+}
+
+function authenticateUser($username, $password) 
+{	
+	$sql = "SELECT password
+			FROM   user
+			WHERE  username = :username";
+	$pdo = new pdo('mysql:host=dbhost.cs.man.ac.uk; dbname=2021_comp10120_z19', 'y02478jh', 'Hjy20011222.');
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute([
+		'username' => $username
+					]);
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$row = $stmt->fetch();
+	if (password_verify($password, $row['password'])){ 
+		$_SESSION['login'] = "logined";
+		header("location: timetable.php");
+	}
+	else{
+		session_destroy();
+		header("location: loginPage.php");
+	}
+}
+?>

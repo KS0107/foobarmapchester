12bc4e8cd6760b2af24b57a04d5fdb4b6846ccdc
Kushagra Srivastava <1kushagra2003@gmail.com>
2022-02-24 13:04:14 +0000

    Progress bar

--- Javascripts/funcs.js
+++ Javascripts/funcs.js
@@ -256,3 +256,10 @@ function slideInOut() {
         z++;
     }
 }
+window.addEventListener('scroll', function() {myFunction()});
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}

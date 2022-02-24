25ca183912f5bff1c389b8361604e88d80ceb772
Jiaying Huang <jiaying.huang-4@student.manchester.ac.uk>
2022-02-24 13:18:20 +0000

    bug

--- webpages/account.php
+++ webpages/account.php
@@ -1,7 +1,7 @@
 <?php
 function addUser($username, $password)
 {	// insert username and password into users table  
-	$sql = "INSERT INTO user (username, password) 
+	$sql = "INSERT INTO user (Username, Password) 
 			VALUES (:username, :password)";
 	// connect to the database
 	$pdo = new pdo('mysql:host=dbhost.cs.man.ac.uk; dbname=2021_comp10120_z19', 'y02478jh', 'i7JLzgM-z5zv9T');

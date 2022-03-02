9ec0dff6ef574c0f3e96498ee0ed9f5be20e7369
George Goodey <george.goodey@student.manchester.ac.uk>
2022-03-02 11:17:59 +0000

    possible button fix

--- Javascripts/funcs.js
+++ Javascripts/funcs.js
@@ -231,29 +231,29 @@ function goToReviews() {
     var path =  window.location.pathname;
     var page = path.split("/").pop();
     if(page == "index.html"){
-        //window.location = "../manchester/webpages/reviewPage.html"
+        window.location = "../manchester/webpages/reviewPage.html"
     }else{
+        window.location = "../webpages/reviewPage.html"
     }
-    window.location = "../webpages/reviewPage.html"
 }
 
 function goToMap() {
     var path =  window.location.pathname;
     var page = path.split("/").pop();
     if(page == "index.html"){
-        window.location = "./webpages/map.html"
+        window.location = "webpages/map.html"
     }else{
-        window.location = "./webpages/map.html"
+        window.location = "../webpages/map.html"
     }
-}
+}//🅱
 
 function goToHome() {
     var path =  window.location.pathname;
     var page = path.split("/").pop();
     if(page == "index.html"){
-        window.location = "./manchester/index.html"
+        window.location = "index.html"
     }else{
-        window.location = "./manchester/index.html"
+        window.location = "../index.html"
     }
 }
 
@@ -261,7 +261,7 @@ function goToTimetable() {
     var path =  window.location.pathname;
     var page = path.split("/").pop();
     if(page == "index.html"){
-        window.location = "../manchester/webpages/timetable.html"
+        window.location = "webpages/timetable.php"
     }else{
         window.location = "../webpages/timetable.php"
     }
@@ -271,7 +271,7 @@ function goToLogin() {
     var path =  window.location.pathname;
     var page = path.split("/").pop();
     if(page == "index.html"){
-        window.location = "../manchester/webpages/loginPage.html"
+        window.location = "webpages/loginPage.php"
     }else{
         window.location = "../webpages/loginPage.php"
     }

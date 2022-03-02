63c0bd337d434baa3c1ea7cf0db73d24d948710b
George Goodey <george.goodey@student.manchester.ac.uk>
2022-03-02 13:00:08 +0000

    funcs

--- Javascripts/funcs.js
+++ Javascripts/funcs.js
@@ -237,7 +237,7 @@ function goToReviews() {
     var path =  window.location.pathname;
     var page = path.split("/").pop();
     if(page == "index.html"){
-        window.location = "../manchester/webpages/reviewPage.html"
+        window.location = "webpages/reviewPage.html"
     }else{
         window.location = "../webpages/reviewPage.html"
     }

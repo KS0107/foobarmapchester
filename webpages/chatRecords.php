beb1dd37c11fe0c5f1a3521e0ffb48d9e222a662
Jiaying Huang <jiaying.huang-4@student.manchester.ac.uk>
2022-03-03 11:44:44 +0000

    message

--- webpages/chatRecords.php
+++ webpages/chatRecords.php
@@ -2,14 +2,14 @@
 
 		readRecords();
 		function readRecords(){
-			$sql = "SELECT date, content
-					FROM chatRecords";
+			$sql = "SELECT Date, Content
+					FROM ChatRecords";
 			$pdo = new pdo("mysql:host=dbhost.cs.man.ac.uk; dbname=2021_comp10120_z19", "y02478jh", "i7JLzgM-z5zv9T");
 			$stmt = $pdo->prepare($sql);
 			$stmt->execute();
 			$stmt->setFetchMode(PDO::FETCH_ASSOC);
 			while ($row = $stmt->fetch()){
-	        	print("<p>" . $row['date'] . " " . $row['content'] . "</p>");
+	        	print("<p>" . $row['Date'] . " " . $row['Content'] . "</p>");
 	    	}
 		}
 	?>

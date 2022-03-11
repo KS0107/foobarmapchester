be8fd1bffd736b595b891fae743b935bbc4043c7
georgegoodey <george.goodey@student.manchester.ac.uk>
2022-03-11 00:43:51 +0000

    Finished Error trapping and timetable funcs

--- webpages/timetable.php
+++ webpages/timetable.php
@@ -63,7 +63,6 @@ function getTimetable($UserID)
 	$stmt = $pdo->prepare($sql);
 	$stmt->execute(["UserID"=>$UserID]);
 	$times = $stmt->fetch(PDO::FETCH_ASSOC);
-	echo $times["10am2pm"];
 	if(gettype($times["10am2pm"]) == "string"){
 		setcookie("time1", $times["10am2pm"]);
 		setcookie("time2", $times["2pm6pm"]);

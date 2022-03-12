33516b508ddeea436295ac6acd779bb55fd1ad44
Jiaying Huang <jiaying.huang-4@student.manchester.ac.uk>
2022-03-12 11:26:16 +0000

    debug

--- webpages/chatFunc.js
+++ webpages/chatFunc.js
@@ -71,7 +71,7 @@ function loadLog(friend){
     xhttp.onload = function() {
     let text = JSON.parse(this.responseText);
     let message="";
-    alert(text[1].MessageBody);
+    alert("text[1].MessageBody: " + text[1].MessageBody + " text[0].Sender: " + text[0].Sender + " text.length: " + text.length + "json: " + this.responseText);
     for(let i = 1; i < text.length; i++){
         if(text[i].UserID == text[0].Sender){
             message += "<div style=\"text-align: right;\">" + text[i].CreateDate + "<br>" + text[i].MessageBody + "</div>";

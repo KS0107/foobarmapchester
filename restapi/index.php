a46010c545bd4069645abdadf46a9267c580b289
Jiaying Huang <jiaying.huang-4@student.manchester.ac.uk>
2022-03-15 20:18:06 +0000

    debugging

--- restapi/index.php
+++ restapi/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/inc/bootstrap.php";
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-if ((isset($uri[5]) && $uri[4] != 'user') || !isset($uri[5])) {
+if ((isset($uri[6]) && $uri[5] != 'user') || !isset($uri[6])) {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
@@ -13,7 +13,7 @@ if ((isset($uri[5]) && $uri[4] != 'user') || !isset($uri[5])) {
 require PROJECT_ROOT_PATH . "/Controller/Api/UserController.php";
 
 $objFeedController = new UserController();
-$strMethodName = $uri[5] . 'Action';
+$strMethodName = $uri[6] . 'Action';
 $objFeedController->{$strMethodName}();
 
 ?>

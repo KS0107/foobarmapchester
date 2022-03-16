99c6946ad9310df772df1a63d18f4240e1e22c4f
Oliver Clements <oliver.clements-2@student.manchester.ac.uk>
2022-03-16 10:45:37 +0000

    pulling locations

--- webpages/map.php
+++ webpages/map.php
@@ -40,3 +40,28 @@
     <div id='map'></div>
 </body>
 </html>
+
+<?php
+    //open connection to mysql db
+    $connection = mysqli_connect('mysql:host=dbhost.cs.man.ac.uk; dbname=2021_comp10120_z19', 'y02478jh', 'i7JLzgM-z5zv9T') or die("Error " . mysqli_error($connection));
+
+    //fetch table rows from mysql db
+    $sql = "SELECT *
+            FROM   Location";
+    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+
+    //create an array
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+    
+    echo json_encode($emparray);
+    //$fp = fopen('empdata.json', 'w');
+    //fwrite($fp, json_encode($emparray));
+    //fclose($fp);
+
+    //close the db connection
+    mysqli_close($connection);
+?>

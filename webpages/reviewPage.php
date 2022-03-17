5b914c3911b32c410b41c77835cdba5b092c22b9
George Goodey <george.goodey@student.manchester.ac.uk>
2022-03-17 22:17:40 +0000

    Review page edits

--- webpages/reviewPage.php
+++ webpages/reviewPage.php
@@ -1,3 +1,49 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Review Page</title>
+    <link rel="stylesheet" href="../CSS/mainstyle.css">
+    <script src="../Javascripts/funcs.js"></script>
+    <script src="../Javascripts/data.js"></script>
+</head>
+<body>
+    <div class="topBar">
+        <div id="buttonLinks">
+            <button id="btnHP">Home Page</button>
+            <button id="btnMP">Map Page</button>
+            <button id="btnRP">Review Page</button>
+            <button id="btnCP">Chat</button>
+            <button id="btnTP">Timetable</button>
+            <button id="btnSP">Sign Out</button>
+        </div>
+    </div>
+    <div id="pageBody">
+        <h4>Enter your review</h4>
+        <form action="reviewPage.php" method="post">
+            <div class="formBox">
+                <p>Location</p>
+                <input type="text" id="location" placeholder="Location" />
+            </div>
+            <div class="formBox">
+                <p>Rating</p>
+                <input type="range" id="rating" max="5" min="1" step="1" />
+            </div>
+            <div class="formBox">
+                <p>Review</p>
+                <textarea style="resize:none" type="text" id="review" placeholder="Write your review!" ></textarea>
+            </div>
+            <div class="formBox">
+                <input id="btnRP" type="submit" name="save" value="Save" />
+            </div>
+    		
+            
+        </form>
+    </div>
+</body>
+</html>
+
 <?php
 function addReview($id, $location, $date, $rating, $review)
 {	 
@@ -9,50 +55,8 @@ function addReview($id, $location, $date, $rating, $review)
     $conn = new mysqli($servername, $username, $password, $dbname);
 	$sql = "INSERT INTO `Reviews` (`id`, `location`, `date`, `rating`, `review`) VALUES ('".$value['id']."', '".$value['location']."', '".$value['date']."', '".$value['rating']."', '".$value['review']."')";
 }
-// function showReviews(){
-//     parse_str($_SERVER["QUERY_STRING"], $query);
-//     $servername = "dbhost.cs.man.ac.uk";
-//     $username = "y02478jh";
-//     $password = "i7JLzgM-z5zv9T";
-//     $dbname = "2021_comp10120_z19";
-//     try {
-//         $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-//         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//         $sqlReview = "SELECT LocationID
-//         FROM Location
-//         WHERE Name = :query";
-//         // $sqlReview = "SELECT date,rating,review
-//         // FROM Reviews
-//         // WHERE LocationID = $sqlReview";
-//         $stmt = $connection->prepare($sqlReview);
-//         $stmt->execute(["query"=>$query["LocationName"]]);
-//     } catch(PDOException $error) {
-//         echo "Connection failed: " . $error->getMessage();
-//     }
-//     $stmt->setFetchMode(PDO::FETCH_ASSOC);
-//     $row= $stmt->fetch();
-
-//     header('Content-Type:application/json');
-//     header('HTTP/1.1 200 OK');
-//     echo json_encode($row["LocationID"]);
-// }
+if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['save']))
+{
+    addR($UserID);
+}
 ?>
-
-
-
-//$filename = "/home/csimage/Documents/Team Project/data.json";
-
-
-//$data = file_get_contents($filename);
-
-
-//$array = json_decode($data,true);
-
-
-//foreach ($array as $value) {
-
-//	$query = "INSERT INTO `jsoninsert` (`id`, `location`, `date`, `rating`, `review`) VALUES ('".$value['id']."', '".$value['location']."', '".$value['date']."', '".$value['rating']."', '".$value['review']."')";
-//	mysqli_query($con,$query);
-//}
-
-//echo "Data Inserted Successfully"

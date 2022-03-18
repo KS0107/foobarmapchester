15e854085f1653aa66f9f9569912da58ba80e7f5
georgegoodey <george.goodey@student.manchester.ac.uk>
2022-03-18 00:06:39 +0000

    Data changes

--- Javascripts/data.js
+++ Javascripts/data.js
@@ -226,7 +226,7 @@ var markers = {'type': 'FeatureCollection',
 };
 
 function generateLocations(){
-    for (let i = 1; i <= 12; i++) {
+    for (let i = 1; i <= 18; i++) {
         locationObj = document.getElementById(i);
         locationData = locationObj.textContent.split(",");
         locationJSON = {

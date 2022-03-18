1d3a593fffd066a5888822b388ccebd6bd77ad60
georgegoodey <george.goodey@student.manchester.ac.uk>
2022-03-18 12:11:29 +0000

    Display changes

--- Javascripts/funcs.js
+++ Javascripts/funcs.js
@@ -13,13 +13,25 @@ function displayReviews(reviews){
         reviewTextBlockOut += ("Reviewed On: " + reviews[i].date + "\n");
         reviewTextBlockOut += ("Given A Score Of: " + reviews[i].rating + " Out Of 5" + "\n");
         reviewActualText = reviews[i].review
-        if(Math.floor(reviewActualText.length / 30) == 1){
-            reviewActualText = reviewActualText.slice(0, 30) + "\n" + reviewActualText.slice(30)
-        }else if(Math.floor(reviewActualText.length / 30) == 2){
-            reviewActualText = reviewActualText.slice(0, 30) + "\n" + reviewActualText.slice(30, 60) + "\n" + reviewActualText.slice(60)
-        }else if(Math.floor(reviewActualText.length / 30) == 3){
-            reviewActualText = reviewActualText.slice(0, 30) + "\n" + reviewActualText.slice(30, 60) + "\n" + reviewActualText.slice(60, 90) + "\n" + reviewActualText.slice(90)
+        for (let i = 0; i < Math.floor(reviewActualText.length / 30); i++) {
+            foundSpace = false;
+            decrement = 0;
+            while(!foundSpace){
+                if(reviewActualText[30*i-decrement] == " "){
+                    reviewActualText = reviewActualText.slice(30*(i-1)-decrement, 30*i-decrement) + "\n" + reviewActualText.slice(30*i-decrement);
+                    foundSpace = true;
+                }else{
+                    decrement ++;
+                }
+            }
         }
+        // if(Math.floor(reviewActualText.length / 30) == 1){
+        //     reviewActualText = reviewActualText.slice(0, 30) + "\n" + reviewActualText.slice(30)
+        // }else if(Math.floor(reviewActualText.length / 30) == 2){
+        //     reviewActualText = reviewActualText.slice(0, 30) + "\n" + reviewActualText.slice(30, 60) + "\n" + reviewActualText.slice(60)
+        // }else if(Math.floor(reviewActualText.length / 30) == 3){
+        //     reviewActualText = reviewActualText.slice(0, 30) + "\n" + reviewActualText.slice(30, 60) + "\n" + reviewActualText.slice(60, 90) + "\n" + reviewActualText.slice(90)
+        // }
         reviewTextBlockOut += ("Review: " + reviewActualText + "\n\n");
     }
     let pre = document.querySelector("#msg pre");

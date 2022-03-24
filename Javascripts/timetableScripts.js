c699f42d2ca26a60a0625c0dcc183b4ee9d11f85
georgegoodey <george.goodey@student.manchester.ac.uk>
2022-03-24 10:27:12 +0000

    Timetable fix

--- Javascripts/timetableScripts.js
+++ Javascripts/timetableScripts.js
@@ -47,7 +47,7 @@ function loadTimetable(timetable){
         for (let i = 0; i < 7; i++) {
             console.log(element[i]);
             var newCell = newRow.insertCell();
-            if(element[i] != null){
+            if(element[i] != undefined){
                 newCell.textContent = element[i];
                 newCell.style.backgroundColor = "rgba(31, 31, 31, 0.6)";
             }else{

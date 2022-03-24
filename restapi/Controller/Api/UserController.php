f760bc97ee74a12d45e5d7c13f46f22c99fe9e66
georgegoodey <george.goodey@student.manchester.ac.uk>
2022-03-24 23:56:37 +0000

    Bug fixes

--- restapi/Controller/Api/UserController.php
+++ restapi/Controller/Api/UserController.php
@@ -468,7 +468,7 @@ class UserController extends BaseController{
                         if($value2 != null){
                             $receiver = $userModel->checkReceiver($userID, $value2, "10am-2pm", "Thu");
                             if(is_array($receiver)){
-                                $value2 = $value2+$receiver[0]["TargetID"];
+                                $value2 = $value2+$receiver["TargetID"];
                             }
                         }
                     }

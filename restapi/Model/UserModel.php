cbc8cd9089f6fe0d8df1fa2180ec21dc0c13d11d
georgegoodey <george.goodey@student.manchester.ac.uk>
2022-03-24 09:46:29 +0000

    Indentation

--- restapi/Model/UserModel.php
+++ restapi/Model/UserModel.php
@@ -361,13 +361,13 @@ class UserModel extends Database{
         return $this->executeQuery($sql, ["userid"=>$userID]);
    }
 
-    public function getTimetable($username){
+   public function getTimetable($username){
         $UserID = getID($username);
         $sql = "SELECT Mon, Tue, Wed, Thu, Fri, Sat, Sun
                 FROM   Timetable
                 WHERE  UserID = :UserID";
         return $this->executeFetchQuery($sql, ["UserID"=>$UserID]);
-    }
+   }
 
 }
 

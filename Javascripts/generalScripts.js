4c5c55e7afc05baaae7ddb59f2fccae449a036c8
Jiaying Huang <jiaying.huang-4@student.manchester.ac.uk>
2022-03-27 14:15:02 +0100

    logout when close the browser

--- Javascripts/generalScripts.js
+++ Javascripts/generalScripts.js
@@ -81,7 +81,18 @@ function goToPage(page){
             console.log("Case didnt work");
     }
     newLink = reconstructLink(pathSections);
+    // made by Jiaying: set a global variable for invoke logout function when close the broswer but not when redireting
+    redirect = true;
     window.location = newLink;
+}
+// When close the page, it invoks logout function.
+window.onbeforeunload = function(){
+    if(!redirect){
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", "../webpages/logoutMedium.php");
+        xmlhttp.send(); 
+        return "are you sure to leave";
+    }
 }
 
 function reconstructLink(sections){
